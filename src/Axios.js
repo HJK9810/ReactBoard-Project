67405ce258f1eeb6d3e18b05b532e2b27222e374
HJ_Kim <50227588+HJK9810@users.noreply.github.com>
2022-10-21 09:06:14 +0900

    for header json

--- src/Axios.js
+++ src/Axios.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const Axios = axios.create({
   baseURL: "http://localhost:8080",
-  headers: {
-    "content-type": "application/json;charset=utf-8",
-  },
   withCredentials: true,
 });
+
+// for json header
+Axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
 
 export default Axios;
